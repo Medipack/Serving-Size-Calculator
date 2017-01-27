eeac9d0c1f1ba034e7db2dac882018128c79a11c
ckc21 <ckc21@sfu.ca>
2017-01-27 13:59:00 -0800

    Initial Commit

---
+++ app/src/main/java/com/example/medipack/servingsizecalculator/Pot.java
@@ -0,0 +1,29 @@
+package com.example.medipack.servingsizecalculator;
+
+/**
+ * Store information about a single pot
+ */
+
+public class Pot {
+
+    // Set member data based on parameters.
+    public Pot(String name, int weightInG) {
+    }
+
+    // Return the weight
+    public int getWeightInG() {
+    }
+
+    // Set the weight. Throws IllegalArgumentException if weight is less than 0.
+    public void setWeightInG(int weightInG) {
+    }
+
+    // Return the name.
+    public String getName() {
+    }
+
+    // Set the name. Throws IllegalArgumentException if name is an empty string (length 0),
+    // or if name is a null-reference.
+    public void setName(String name) {
+    }
+}
