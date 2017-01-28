ab9b4eded55ea3fa970c7fa0ff6c08860c6b2693
Calvin Chou <ckc21@sfu.ca>
2017-01-27 19:03:30 -0800

    Added third activity, calculateServing

--- app/src/main/java/com/example/medipack/servingsizecalculator/calculateServing.java
+++ app/src/main/java/com/example/medipack/servingsizecalculator/calculateServing.java
@@ -2,18 +2,12 @@ package com.example.medipack.servingsizecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
-
-    PotCollection potList;
-
+public class calculateServing extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_calculate_serving);
     }
 }
