6dc8a664aa0aba21aad89390e0ac2901693ccb5d
Calvin Chou <ckc21@sfu.ca>
2017-02-02 23:14:33 -0800

    Implemented listView item click functionality Item click new starts activity for calculate serving size

--- app/src/main/java/com/example/medipack/servingsizecalculator/MainActivity.java
+++ app/src/main/java/com/example/medipack/servingsizecalculator/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,39 +17,61 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "PotCalc";
     PotCollection potList;
+    ListView list;
     //listView : {views: pots.xml}
-
+    //Test
     Pot test = new Pot("test", 70);
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Creates the list
         potList = new PotCollection();
         potList.addPot(test);
-        populateListView();
+        //Populate the list
+        populateListView(list);
 
+        //Make items clickable
+        itemsClickabale();
+
+        //Add Pot button
         addPotLaunch();
     }
 
+    //Make items clickable
+    private void itemsClickabale() {
+        ListView list = (ListView) findViewById(R.id.potList);
+        //set list items clickable
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i(TAG, "Item clicked");
+                //Launches pot calculation activity
+                Intent potCalcIntent = calculateServing.makeIntent(MainActivity.this);
+                startActivity(potCalcIntent);
+            }
+        });
+    }
+
+    //Add Pot button
     private void addPotLaunch() {
         Button addPotBtn = (Button) findViewById(R.id.addPot);
         addPotBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Log the button press
                 Log.i(TAG, "Add pot button clicked");
-
-                // Launch add_a_Pot activity
-                //Intent intent = new Intent(MainActivity.this,addAPot.class);
+                // Launches addAPot activity
                 Intent addPotIntent = addAPot.makeIntent(MainActivity.this);
                 startActivity(addPotIntent);
             }
         });
     }
 
-    private void populateListView() {
+    //Populate a list
+    private void populateListView(ListView list) {
         // Create list of items
         String[] potItems = potList.getPotDescriptions();
 
@@ -59,12 +82,8 @@ public class MainActivity extends AppCompatActivity {
                 potItems);              // Items to be displays
 
         //Configure the list view
-        ListView list = (ListView) findViewById(R.id.potList);
+        list = (ListView) findViewById(R.id.potList);
         list.setAdapter(adapter);
     }
 
-    /*public void additionalPot(View view){
-        Intent intent = new Intent(this,addAPot.class);
-
-    }*/
 }
